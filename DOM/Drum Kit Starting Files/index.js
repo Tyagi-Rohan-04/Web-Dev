37f72496a99fcea679564d423110a3ffa21761ec
Rohan <tyagirohan798@gmail.com>
2025-10-12 11:06:05 +0530

    Drum kit project file addition

---
+++ DOM/Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(){
+    alert("I got clicked!");
+}
+
+function handleClick(){
+    alert("I got clicked!");
+}
