15115492bb3c3a804c9df04e15aac8ccfd674bfd
Rohan <tyagirohan798@gmail.com>
2025-11-21 02:23:52 +0530

    adding event listners using jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,18 @@
 /* $("h1").text("Bye");
-$("button").html("<em>Hey</em>"); */
+$("button").html("<em>Hey</em>"); 
 
-$("a").attr("href", "https://www.yahoo.com");
+$("a").attr("href", "https://www.yahoo.com"); */
+
+/* $("h1").click(function(){
+    $("h1").css("color", "purple");
+}); 
+*/
+/* $("button").click(function(){
+    $("h1").css("color", "purple");
+}); */
+/* $(document).keypress(function(event){
+    $("h1").text(event.key);
+}); */
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "purple");
+});
