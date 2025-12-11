94b98bbd917da917383a2cf5d3f4c17faa300e32
Rohan <tyagirohan798@gmail.com>
2025-12-11 22:11:52 +0530

    EJS lesson, initial files addition

--- Node/4.0 EJS/index.js
+++ Node/4.0 EJS/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>");
+    res.render("index.ejs", {
+        dayType: "a weekday",
+        advice: "it's time to work hard",
+    });
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
-})
+});
